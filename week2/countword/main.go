dcb43d8f5b4e54ff97029dd8b7350d03b51636d8
Quang Le <iamquang95@gmail.com>
2019-07-24 22:48:44 +0700

    Count words without handling stop function

--- week2/countword/main.go
+++ week2/countword/main.go
@@ -10,9 +10,42 @@ const (
 	dataRoot = "data"
 )
 
-func printFileData(file chan file.FileData) {
-	for data := range file {
+func printFileData(files chan file.FileData) {
+	for data := range files {
 		fmt.Println(data.Words)
+	}
+}
+
+type result struct {
+	wordsCount map[string]int
+}
+
+func countWords(file file.FileData) (result, error) {
+	cnt := make(map[string]int)
+	if file.Err != nil {
+		return result{cnt}, file.Err
+	}
+	for _, word := range file.Words {
+		cnt[word]++
+	}
+	return result{cnt}, nil
+}
+
+func countWordsInFiles(files chan file.FileData, counts chan result) {
+	for data := range files {
+		// TODO: Handle error here
+		wordsCnt, _ := countWords(data)
+		counts <- wordsCnt
+	}
+}
+
+func collectResults(counts chan result) {
+	res := make(map[string]int)
+	for count := range counts {
+		for k, v := range count.wordsCount {
+			res[k] = res[k] + v
+		}
+		fmt.Println(res)
 	}
 }
 
@@ -21,6 +54,9 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to read files: ", err)
 	}
-	go printFileData(in)
+	// go printFileData(in)
+	counts := make(chan result)
+	go countWordsInFiles(in, counts)
+	go collectResults(counts)
 	fmt.Scanln()
 }
