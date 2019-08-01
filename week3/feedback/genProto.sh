3c866af07d9923cff26ab0246c9f406d7569e199
Quang Le <iamquang95@gmail.com>
2019-08-01 20:25:40 +0700

    Add HTTP server using grpc_gateway

--- week3/feedback/genProto.sh
+++ week3/feedback/genProto.sh
@@ -1,1 +1,12 @@
-protoc -I feedback/ feedback/feedback.proto --go_out=plugins=grpc:feedback
+# protoc -I feedback/ feedback/feedback.proto --go_out=plugins=grpc:feedback
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=plugins=grpc:. \
+  feedback/feedback.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:. \
+  feedback/feedback.proto
